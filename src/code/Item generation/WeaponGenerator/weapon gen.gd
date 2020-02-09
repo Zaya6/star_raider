4963054c29e5f3b63a7642ebc33ce7bed0b83c64
Sinist75 <sinist75gaming@gmail.com>
2020-02-08 19:03:13 -0600

    Delete weapon gen.gd

--- src/code/Item generation/WeaponGenerator/weapon gen.gd
+++
@@ -1,7 +0,0 @@
-#This code was written by Robert Griffin (aka Sinist. aka Sinist75) from December 2019 to january 2020
-extends Panel
-
-func _ready():
-	var wepGen = WepGeneration.new() #creates a weapon that has three properties. 
-	print(wepGen.getFullWeapon())
-	pass
